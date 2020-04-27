a8f65bf40de94f9ce1b4a5c534e42dd68bb0594a
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-27 18:40:17 +0900

    add test action

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import core from '@actions/core';
+
+const run = async () => {
+  try {
+    core.setOutput('has_update', 'yes');
+    core.setOutput('formatted_as_json', '[]');
+    core.setOutput('formatted_as_columns', '|--|');
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+};
+
+export default run;
